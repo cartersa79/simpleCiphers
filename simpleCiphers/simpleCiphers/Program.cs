dc2c55c350332062d57f74f22d662d0fb4648926
cartersa79 <38816845+cartersa79@users.noreply.github.com>
2019-07-30 18:16:41 -0400

    finished jeffersonWheelCipher. I still need to go back, clean up, and double check everything. Also, I need to clean up the main() a little.

--- simpleCiphers/simpleCiphers/Program.cs
+++ simpleCiphers/simpleCiphers/Program.cs
@@ -6,25 +6,31 @@ namespace simpleCiphers
     {
         static void Main(string[] args)
         {
-            // test railFenceCipher encipher and decipher
-            railFenceCipher myTest = new railFenceCipher();
-            string inputText = "CSC260 was a really great $%$## class!!!";
-            string encipheredTest = myTest.encipher(inputText, 3);
-            Console.WriteLine(encipheredTest);
-            string decipheredText = myTest.decipher(inputText, 3);
-            Console.WriteLine(decipheredText);
-            Console.WriteLine("\n");
+            //// test railFenceCipher encipher and decipher
+            //railFenceCipher myTest = new railFenceCipher();
+            //string inputText = "CSC260 was a really great $%$## class!!!";
+            //string encipheredTest = myTest.encipher(inputText, 3);
+            //Console.WriteLine(encipheredTest);
+            //string decipheredText = myTest.decipher(inputText, 3);
+            //Console.WriteLine(decipheredText);
+            //Console.WriteLine("\n");
 
-            // test Caesar cipher
-            caesarCipher testCaesar = new caesarCipher();
-            string caesarTestInput = "I truly enjoyed the course, Thanks!";
-            string caesarEncipheredTest = testCaesar.encipher(caesarTestInput, 1);
-            Console.WriteLine(caesarEncipheredTest);
-            Console.WriteLine(testCaesar.decipher(caesarEncipheredTest, 1));
-            Console.WriteLine("\n");
-            testCaesar.bruteForceDecipher(caesarEncipheredTest);
+            //// test Caesar cipher
+            //caesarCipher testCaesar = new caesarCipher();
+            //string caesarTestInput = "I truly enjoyed the course, Thanks!";
+            //string caesarEncipheredTest = testCaesar.encipher(caesarTestInput, 1);
+            //Console.WriteLine(caesarEncipheredTest);
+            //Console.WriteLine(testCaesar.decipher(caesarEncipheredTest, 1));
+            //Console.WriteLine("\n");
+            //testCaesar.bruteForceDecipher(caesarEncipheredTest);
 
-
+            // test Jefferson cipher
+            jeffersonWheelCipher testTJ = new jeffersonWheelCipher();
+            string wheelTestInput = "Hello World!";
+            string wheelTestEnciphered = testTJ.encipher(wheelTestInput, 1);
+            Console.WriteLine(wheelTestEnciphered);
+            string wheelTestDeciphered = testTJ.decipher(wheelTestEnciphered, 1);
+            Console.WriteLine(wheelTestDeciphered);
 
 
 
--- simpleCiphers/simpleCiphers/jeffersonWheelCipher.cs
+++ simpleCiphers/simpleCiphers/jeffersonWheelCipher.cs
@@ -19,6 +19,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace simpleCiphers
 {
@@ -26,28 +27,81 @@ namespace simpleCiphers
     {
 
         // fields
+        int numWheels = 10;
 
-        char[] w0 = { 'A', 'B', 'C', 'E', 'I', 'G', 'D', 'J', 'F', 'V', 'U', 'Y', 'M', 'H', 'T', 'Q', 'K', 'Z', 'O', 'L', 'R', 'X', 'S', 'P', 'W', 'N' };
-        char[] w1 = { 'A', 'C', 'D', 'E', 'H', 'F', 'I', 'J', 'K', 'T', 'L', 'M', 'O', 'U', 'V', 'Y', 'G', 'Z', 'N', 'P', 'Q', 'X', 'R', 'W', 'S', 'B' };
-        char[] w2 = { 'A', 'D', 'K', 'O', 'M', 'J', 'U', 'B', 'G', 'E', 'P', 'H', 'S', 'C', 'Z', 'I', 'N', 'X', 'F', 'Y', 'Q', 'R', 'T', 'V', 'W', 'L' };
-        char[] w3 = { 'A', 'E', 'D', 'C', 'B', 'I', 'F', 'G', 'J', 'H', 'L', 'K', 'M', 'R', 'U', 'O', 'Q', 'V', 'P', 'T', 'N', 'W', 'Y', 'X', 'Z', 'S' };
-        char[] w4 = { 'A', 'F', 'N', 'Q', 'U', 'K', 'D', 'O', 'P', 'I', 'T', 'J', 'B', 'R', 'H', 'C', 'Y', 'S', 'L', 'W', 'E', 'M', 'Z', 'V', 'X', 'G' };
-        char[] w5 = { 'A', 'G', 'P', 'O', 'C', 'I', 'X', 'L', 'U', 'R', 'N', 'D', 'Y', 'Z', 'H', 'W', 'B', 'J', 'S', 'Q', 'F', 'K', 'V', 'M', 'E', 'T' };
-        char[] w6 = { 'A', 'H', 'X', 'J', 'E', 'Z', 'B', 'N', 'I', 'K', 'P', 'V', 'R', 'O', 'G', 'S', 'Y', 'D', 'U', 'L', 'C', 'F', 'M', 'Q', 'T', 'W' };
-        char[] w7 = { 'A', 'I', 'H', 'P', 'J', 'O', 'B', 'W', 'K', 'C', 'V', 'F', 'Z', 'L', 'Q', 'E', 'R', 'Y', 'N', 'S', 'U', 'M', 'G', 'T', 'D', 'X' };
-        char[] w8 = { 'A', 'J', 'D', 'S', 'K', 'Q', 'O', 'I', 'V', 'T', 'Z', 'E', 'F', 'H', 'G', 'Y', 'U', 'N', 'L', 'P', 'M', 'B', 'X', 'W', 'C', 'R' };
-        char[] w9 = { 'A', 'K', 'E', 'L', 'B', 'D', 'F', 'J', 'G', 'H', 'O', 'N', 'M', 'T', 'P', 'R', 'Q', 'S', 'V', 'Z', 'U', 'X', 'Y', 'W', 'I', 'C' };
+        private char[,] wheels = {
+            { 'A', 'B', 'C', 'E', 'I', 'G', 'D', 'J', 'F', 'V', 'U', 'Y', 'M', 'H', 'T', 'Q', 'K', 'Z', 'O', 'L', 'R', 'X', 'S', 'P', 'W', 'N' },
+            { 'A', 'C', 'D', 'E', 'H', 'F', 'I', 'J', 'K', 'T', 'L', 'M', 'O', 'U', 'V', 'Y', 'G', 'Z', 'N', 'P', 'Q', 'X', 'R', 'W', 'S', 'B' },
+            { 'A', 'D', 'K', 'O', 'M', 'J', 'U', 'B', 'G', 'E', 'P', 'H', 'S', 'C', 'Z', 'I', 'N', 'X', 'F', 'Y', 'Q', 'R', 'T', 'V', 'W', 'L' },
+            { 'A', 'E', 'D', 'C', 'B', 'I', 'F', 'G', 'J', 'H', 'L', 'K', 'M', 'R', 'U', 'O', 'Q', 'V', 'P', 'T', 'N', 'W', 'Y', 'X', 'Z', 'S' },
+            { 'A', 'F', 'N', 'Q', 'U', 'K', 'D', 'O', 'P', 'I', 'T', 'J', 'B', 'R', 'H', 'C', 'Y', 'S', 'L', 'W', 'E', 'M', 'Z', 'V', 'X', 'G' },
+            { 'A', 'G', 'P', 'O', 'C', 'I', 'X', 'L', 'U', 'R', 'N', 'D', 'Y', 'Z', 'H', 'W', 'B', 'J', 'S', 'Q', 'F', 'K', 'V', 'M', 'E', 'T' },
+            { 'A', 'H', 'X', 'J', 'E', 'Z', 'B', 'N', 'I', 'K', 'P', 'V', 'R', 'O', 'G', 'S', 'Y', 'D', 'U', 'L', 'C', 'F', 'M', 'Q', 'T', 'W' },
+            { 'A', 'I', 'H', 'P', 'J', 'O', 'B', 'W', 'K', 'C', 'V', 'F', 'Z', 'L', 'Q', 'E', 'R', 'Y', 'N', 'S', 'U', 'M', 'G', 'T', 'D', 'X' },
+            { 'A', 'J', 'D', 'S', 'K', 'Q', 'O', 'I', 'V', 'T', 'Z', 'E', 'F', 'H', 'G', 'Y', 'U', 'N', 'L', 'P', 'M', 'B', 'X', 'W', 'C', 'R' },
+            { 'A', 'K', 'E', 'L', 'B', 'D', 'F', 'J', 'G', 'H', 'O', 'N', 'M', 'T', 'P', 'R', 'Q', 'S', 'V', 'Z', 'U', 'X', 'Y', 'W', 'I', 'C' }
+        };
 
-        
-       
 
         // getters and setters
-        // default
+        // none
 
         // constructors
         // default
 
         // methods
+
+        public override string cleanText(string inputText)
+        {
+            inputText = inputText.ToUpper();
+            inputText = Regex.Replace(inputText, @"[^A-Z]", string.Empty);
+
+            // reduce string to the number of wheels
+            if (inputText.Length > numWheels)
+                inputText = inputText.Substring(0, numWheels - 1);
+
+            return inputText;
+        }
+
+
+        public string encipher(string inputText, int key)
+        {
+            inputText = this.cleanText(inputText);
+            StringBuilder sbResult = new StringBuilder();
+
+            for (int row = 0; row < numWheels; row++)   // each wheel
+            {
+                for (int col = 0; col < 26; col++)      // each letter on wheel
+                {
+                    if (wheels[row, col] == inputText[row])
+                        sbResult.Append(wheels[row, ((col + key) % 26)]);
+                }
+            }
+            return sbResult.ToString();
+        }
+
+        public string decipher(string inputText, int key)
+        {
+            return encipher(inputText, (26 + (-1 * key)));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
